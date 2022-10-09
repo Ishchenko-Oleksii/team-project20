377f738af6e4ab3afcfabfb449c67ae4b8b31ebb
Nataliia-Bilets <natalia_bilets@ukr.net>
2022-10-09 16:38:53 +0300

    html gallery

---
+++ src/index.js
@@ -0,0 +1,30 @@
+new Swiper('.image-slider', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+  mousewheel: {
+    sensitivity: 1,
+  },
+});
+new Swiper('.reviews-slider', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+  mousewheel: {
+    sensitivity: 1,
+  },
+});
