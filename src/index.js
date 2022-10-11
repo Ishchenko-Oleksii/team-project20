1990b2fa19d28c8af90568acd1b942cb2558276c
Nataliia-Bilets <natalia_bilets@ukr.net>
2022-10-11 18:57:25 +0300

    commit gallery & backgraund

--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
 new Swiper('.image-slider', {
-  // navigation: {
-  //   nextEl: '.swiper-button-next',
-  //   prevEl: '.swiper-button-prev',
-  // },
-
-  // pagination: {
-  //   el: '.swiper-pagination',
-  //   clickable: true,
-  //   dynamicBullets: true,
-  // },
-  // mousewheel: {
-  //   sensitivity: 1,
-  // },
+  mousewheel: {
+    sensitivity: 1,
+  },
   autoplay: {
     delay: 2000,
     disableOnInteraction: false,
@@ -31,29 +21,12 @@ new Swiper('.image-slider', {
 });
 
 new Swiper('.reviews-slider', {
-  // navigation: {
-  //   nextEl: '.swiper-button-next',
-  //   prevEl: '.swiper-button-prev',
-  // },
-
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
     // dynamicBullets: true,
   },
-  // mousewheel: {
-  //   sensitivity: 1,
-  // },
-  // autoplay: {
-  //   delay: 5000,
-  //   disableOnInteraction: false,
-  // },
-  spped: 1000,
-
-  effect: 'flip',
-  flipEffect: {
-    slideShadows: true,
-    limitRotation: true,
+  mousewheel: {
+    sensitivity: 1,
   },
-
 });
