5bc4cebe4b48a3b8d4a93a5cb8df4be5f7de4194
Nataliia-Bilets <natalia_bilets@ukr.net>
2022-10-11 11:33:58 +0300

    fixed bgi

--- src/index.js
+++ src/index.js
@@ -1,31 +1,59 @@
 new Swiper('.image-slider', {
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+  // navigation: {
+  //   nextEl: '.swiper-button-next',
+  //   prevEl: '.swiper-button-prev',
+  // },
 
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-    dynamicBullets: true,
+  // pagination: {
+  //   el: '.swiper-pagination',
+  //   clickable: true,
+  //   dynamicBullets: true,
+  // },
+  // mousewheel: {
+  //   sensitivity: 1,
+  // },
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
   },
-  mousewheel: {
-    sensitivity: 1,
+  // effect: 'flip',
+  // flipEffect: {
+  //   slideShadows: true,
+  //   limitRotation: true,
+  // },
+    effect: 'cube',
+    flipEffect: {
+    slideShadows: true,
+    shadow: true,
+    shadowOffset: 20,
+    shadowScale: 0.94,
   },
 });
 
 new Swiper('.reviews-slider', {
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+  // navigation: {
+  //   nextEl: '.swiper-button-next',
+  //   prevEl: '.swiper-button-prev',
+  // },
 
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
-    dynamicBullets: true,
+    // dynamicBullets: true,
   },
-  mousewheel: {
-    sensitivity: 1,
+  // mousewheel: {
+  //   sensitivity: 1,
+  // },
+  // autoplay: {
+  //   delay: 5000,
+  //   disableOnInteraction: false,
+  // },
+  spped: 1000,
+
+  effect: 'flip',
+  flipEffect: {
+    slideShadows: true,
+    limitRotation: true,
   },
+
 });
