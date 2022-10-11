c9e6d217de41054ebbdef841bdfae728879f220e
Ishchenko-Oleksii <110259763+Ishchenko-Oleksii@users.noreply.github.com>
2022-10-11 08:38:39 +0000

    Deploying to gh-pages from @ Ishchenko-Oleksii/team-project20@e8978147fdf6cd7a40e5a2495bdff2f0cd8a6c09 🚀

---
+++ index.f70a55ea.js
@@ -0,0 +1,2 @@
+new Swiper(".image-slider",{autoplay:{delay:2e3,disableOnInteraction:!1},effect:"cube",flipEffect:{slideShadows:!0,shadow:!0,shadowOffset:20,shadowScale:.94}}),new Swiper(".reviews-slider",{pagination:{el:".swiper-pagination",clickable:!0},spped:1e3,effect:"flip",flipEffect:{slideShadows:!0,limitRotation:!0}});
+//# sourceMappingURL=index.f70a55ea.js.map
